3c320b938d0d63d8c4f66ed526451f636f4c24e3
dshelters <dshelters@gmail.com>
2017-02-28 20:00:36 -0800

    Further clean up code

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,21 +1,27 @@
+const fs = require('fs');
+
 var results = []; 
 
+var headers = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json'
+};
+
+var sendResponse = function(response, statusCode, data) {
+  response.writeHead(statusCode, headers);
+  response.end(data);
+};
 
 var requestHandler = function(request, response) {
-  var defaultCorsHeaders = {
-    'access-control-allow-origin': '*',
-    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'access-control-allow-headers': 'content-type, accept',
-    'access-control-max-age': 10 // Seconds.
-  };
 
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
   
 
   // declaring some variables
   var statusCode;
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = 'application/json';
   var method = request.method;
   var url = request.url;
   var urlMatch = (url === '/classes/messages' || url === '/classes/room');
@@ -33,34 +39,33 @@ var requestHandler = function(request, response) {
         results.push(result);
       }
     });
-    response.writeHead(statusCode, headers);
-    response.end(JSON.stringify(results[results.length - 1]));
+   
+    sendResponse(response, statusCode, JSON.stringify(results[results.length - 1]));
 
 
   //GET method
   } else if (request.method === 'GET' && urlMatch) {
     statusCode = 200;
-    response.writeHead(statusCode, headers);
     var responseBody = {
       method: method,
       url: url,
       results: results
     };
-    response.end(JSON.stringify(responseBody));
+    
+    sendResponse(response, statusCode, JSON.stringify(responseBody));
 
 
   // options
   } else if (request.method === 'OPTIONS' && urlMatch) {
     statusCode = 200;
-    response.writeHead(statusCode, headers);
     console.log('headers', headers);
-    response.end();
+    
+    sendResponse(response, statusCode);
 
   // different url
   } else {
     statusCode = 404;
-    response.writeHead(statusCode, headers);
-    response.end();
+    sendResponse(response, statusCode);    
   }
   
 };
